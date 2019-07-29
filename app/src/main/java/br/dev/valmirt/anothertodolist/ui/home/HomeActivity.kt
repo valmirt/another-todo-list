77b75e53ff43cca9823e6853d810853f9612c284
Valmir Torres <valmirjr_20@hotmail.com>
2019-07-29 16:44:25 -0300

    home view

--- app/src/main/java/br/dev/valmirt/anothertodolist/ui/home/HomeActivity.kt
+++ app/src/main/java/br/dev/valmirt/anothertodolist/ui/home/HomeActivity.kt
@@ -3,13 +3,11 @@ package br.dev.valmirt.anothertodolist.ui.home
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import br.dev.valmirt.anothertodolist.R
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -29,51 +27,38 @@ class HomeActivity: AppCompatActivity() {
         // Set up Action Bar
         val navController = host.navController
 
-//        val drawerLayout : DrawerLayout? = findViewById(R.id.drawer_layout)
-//        appBarConfiguration = AppBarConfiguration(
-//            setOf(R.id.home_dest, R.id.deeplink_dest),
-//            drawerLayout)
+        val drawerLayout : DrawerLayout? = findViewById(R.id.drawer_layout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home_dest, R.id.statistic_dest),
+            drawerLayout)
 
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBar(navController, appBarConfiguration)
 
-//        setupActionBar(navController, appBarConfiguration)
-
-//        setupNavigationMenu(navController)
+        setupNavigationMenu(navController)
 
         setupBottomNavMenu(navController)
     }
 
 
-//    private fun setupActionBar(navController: NavController,
-//                               appBarConfig : AppBarConfiguration) {
-//        // This allows NavigationUI to decide what label to show in the action bar
-//        // By using appBarConfig, it will also determine whether to
-//        // show the up arrow or drawer menu icon
-////        setupActionBarWithNavController(navController, appBarConfig)
-//    }
+    private fun setupActionBar(navController: NavController,
+                               appBarConfig : AppBarConfiguration) {
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
 
     private fun setupBottomNavMenu(navController: NavController) {
         bottom_nav_view?.setupWithNavController(navController)
     }
 
-//    private fun setupNavigationMenu(navController: NavController) {
-//        // In split screen mode, you can drag this view out from the left
-//        // This does NOT modify the actionbar
-//        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
-//        sideNavView?.setupWithNavController(navController)
-//    }
+    private fun setupNavigationMenu(navController: NavController) {
+        nav_view?.setupWithNavController(navController)
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Have the NavigationUI look for an action or destination matching the menu
-        // item id and navigate there if found.
-        // Otherwise, bubble up to the parent.
         return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
                 || super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        // Allows NavigationUI to support proper up navigation or the drawer layout
-        // drawer menu, depending on the situation
         return findNavController(R.id.my_nav_host_fragment).navigateUp(appBarConfiguration)
     }
 }
