4ad7670c7a32b4a8528c1557fee68ff7b46de559
valmirt <valmirjr_20@hotmail.com>
2019-08-01 23:45:14 -0300

    icon, colors, styles + fix

--- app/src/main/java/br/dev/valmirt/anothertodolist/ui/splash/SplashActivity.kt
+++ app/src/main/java/br/dev/valmirt/anothertodolist/ui/splash/SplashActivity.kt
@@ -2,6 +2,7 @@ package br.dev.valmirt.anothertodolist.ui.splash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import br.dev.valmirt.anothertodolist.R
 
@@ -10,6 +11,8 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorSplash)
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.my_fragment).navigateUp()
