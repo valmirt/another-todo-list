16ba5565286b0f9e0cd7cc592a8c7577476f7a3c
Valmir Torres <valmirjr_20@hotmail.com>
2019-08-07 16:29:22 -0300

    statistic

--- app/src/main/java/br/dev/valmirt/anothertodolist/ui/home/statistic/StatisticFragment.kt
+++ app/src/main/java/br/dev/valmirt/anothertodolist/ui/home/statistic/StatisticFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import br.dev.valmirt.anothertodolist.R
+import kotlinx.android.synthetic.main.fragment_statistic.*
 
 class StatisticFragment: Fragment() {
 
@@ -20,5 +22,25 @@ class StatisticFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_statistic, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.emptyTasks.observe(this, Observer {
+            if (it) default_text.visibility = View.VISIBLE
+        })
+
+        viewModel.activeResult.observe(this, Observer {
+            active_tasks.text = getString(R.string.active_statistic, it)
+        })
+
+        viewModel.completedResult.observe(this, Observer {
+            completed_tasks.text = getString(R.string.completed_statistic, it)
+        })
+
+        viewModel.totalResult.observe(this, Observer {
+            total_tasks.text = getString(R.string.total_statistic, it)
+        })
     }
 }
--- app/src/main/java/br/dev/valmirt/anothertodolist/ui/home/statistic/StatisticViewModel.kt
+++ app/src/main/java/br/dev/valmirt/anothertodolist/ui/home/statistic/StatisticViewModel.kt
@@ -1,6 +1,38 @@
 package br.dev.valmirt.anothertodolist.ui.home.statistic
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.dev.valmirt.anothertodolist.repository.TaskRepository
+import kotlinx.coroutines.launch
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class StatisticViewModel: ViewModel() {
+class StatisticViewModel: ViewModel(), KoinComponent {
+
+    private val taskRepository: TaskRepository by inject()
+
+    val emptyTasks: MutableLiveData<Boolean> = MutableLiveData()
+
+    val activeResult: MutableLiveData<Float> = MutableLiveData()
+
+    val totalResult: MutableLiveData<String> = MutableLiveData()
+
+    val completedResult: MutableLiveData<Float> = MutableLiveData()
+
+    init {
+        viewModelScope.launch {
+            val response = taskRepository.getAllTasks().value
+            val total = response.size
+            val active = response.count { !it.isComplete }
+
+
+            if (total == 0) emptyTasks.value = true
+            else {
+                totalResult.value = total.toString()
+                activeResult.value = 100f * active / total
+                completedResult.value = 100f * (total - active) / total
+            }
+        }
+    }
 }
